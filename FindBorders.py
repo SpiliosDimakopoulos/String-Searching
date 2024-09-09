28008bbc677b722dbe4e5fd774a92c73422be4a3
Spilios Dimakopoulos <150211937+SpiliosDimakopoulos@users.noreply.github.com>
2024-09-09 10:03:04 +0300

    Create FindBorders.py

---
+++ FindBorders.py
@@ -0,0 +1,16 @@
+def FindBorders(p):
+    m = len(p)
+    b = [0] * (m + 1) 
+    j = 0 
+
+    b[0] = j 
+    b[1] = j 
+
+    for i in range(1, m):
+        while j > 0 and p[j] != p[i]:
+            j = b[j]
+        if p[j] == p[i]:
+            j += 1
+        b[i + 1] = j
+
+    return b
